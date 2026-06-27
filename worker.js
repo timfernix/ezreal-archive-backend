d89ad52b6d577ad831ee4778b0dc21804586beb9
timfernix <46400905+timfernix@users.noreply.github.com>
2026-06-27 13:14:55 +0200

    add external links

--- worker.js
+++ worker.js
@@ -39,20 +39,50 @@ export default {
                         s.release_year,
                         (
                             SELECT json_group_array(json_object(
-                                'type', a.type, 
-                                'url', a.r2_key, 
-                                'title', COALESCE(a.title, a.r2_key),
-                                'category', a.category, 
-                                'game', a.game,
-                                'assetReleaseYear', a.asset_release_year,
-                                'tags', (
-                                    SELECT json_group_array(t.name) 
-                                    FROM tags t 
-                                    JOIN asset_tags at ON t.id = at.tag_id 
-                                    WHERE at.asset_id = a.id
-                                )
-                            )) 
-                            FROM assets a WHERE a.skin_id = s.id
+                                'type', m.type,
+                                'url', m.url,
+                                'title', m.title,
+                                'category', m.category,
+                                'game', m.game,
+                                'assetReleaseYear', m.assetReleaseYear,
+                                'tags', m.tags,
+                                'platform', m.platform,
+                                'source', m.source
+                            ))
+                            FROM (
+                                SELECT
+                                    a.type AS type,
+                                    a.r2_key AS url,
+                                    COALESCE(a.title, a.r2_key) AS title,
+                                    a.category AS category,
+                                    a.game AS game,
+                                    a.asset_release_year AS assetReleaseYear,
+                                    (
+                                        SELECT json_group_array(t.name)
+                                        FROM tags t
+                                        JOIN asset_tags at ON t.id = at.tag_id
+                                        WHERE at.asset_id = a.id
+                                    ) AS tags,
+                                    NULL AS platform,
+                                    'asset' AS source
+                                FROM assets a
+                                WHERE a.skin_id = s.id
+
+                                UNION ALL
+
+                                SELECT
+                                    'external' AS type,
+                                    el.url AS url,
+                                    COALESCE(el.title, el.url) AS title,
+                                    el.category AS category,
+                                    el.game AS game,
+                                    el.asset_release_year AS assetReleaseYear,
+                                    '[]' AS tags,
+                                    el.platform AS platform,
+                                    'external_link' AS source
+                                FROM external_links el
+                                WHERE el.skin_id = s.id
+                            ) m
                         ) as media
                     FROM skins s
                 `).all();
